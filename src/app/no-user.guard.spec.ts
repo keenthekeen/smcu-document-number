12a6d4b48a76c9e054ce6bdc4988fe1d5c4b6d23
Sarun Intaralawan <sarunint@docchula.com>
2021-02-07 06:23:41 +0700

    chore(deps): update angular and angular cli packages

--- src/app/no-user.guard.spec.ts
+++ src/app/no-user.guard.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject, waitForAsync } from '@angular/core/testing';
 
 import { NoUserGuard } from './no-user.guard';
 
