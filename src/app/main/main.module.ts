7e36aeb677a4f627e2a91c11d11fa6b4eea0ee68
Siwat Techavoranant <keen@keendev.net>
2021-03-28 12:27:51 +0700

    feat: add DOMPurify

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,9 +5,10 @@ import { MainRoutingModule } from './main-routing.module';
 import { ListComponent } from './list/list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TimePipe } from './time.pipe';
+import { NgDompurifyModule } from '@tinkoff/ng-dompurify';
 
 @NgModule({
-  imports: [CommonModule, MainRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, MainRoutingModule, NgDompurifyModule, ReactiveFormsModule],
   declarations: [ListComponent, TimePipe]
 })
 export class MainModule {}
